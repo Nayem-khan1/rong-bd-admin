26933b44155f5201e6cc697eb40d14a27694fdab
Nayem-khan1 <nayemkhan4496@gmail.com>
2025-05-26 14:10:59 +0600

    enhance order page

---
+++ src/components/tables/OrderTable.jsx
@@ -0,0 +1,124 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from "../ui/table/index";
+import Badge from "../ui/badge/Badge";
+
+import { PencilIcon, TrashBinIcon } from "../../icons";
+
+const OrderTable = ({ data }) => {
+  console.log(data);
+  return (
+    <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
+      <div className="max-w-full overflow-x-auto">
+        <Table>
+          {/* Table Header */}
+          <TableHeader className="border-b border-gray-100 dark:border-white/[0.05]">
+            <TableRow>
+              <TableCell
+                isHeader
+                className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+              >
+                Order Items
+              </TableCell>
+              <TableCell
+                isHeader
+                className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+              >
+                Customer details
+              </TableCell>
+              <TableCell
+                isHeader
+                className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+              >
+                Pay Amount
+              </TableCell>
+              <TableCell
+                isHeader
+                className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+              >
+                Payment
+              </TableCell>
+              <TableCell
+                isHeader
+                className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+              >
+                Payment Method
+              </TableCell>
+              <TableCell
+                isHeader
+                className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+              >
+                Date
+              </TableCell>
+              <TableCell
+                isHeader
+                className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+              >
+                Status
+              </TableCell>
+            </TableRow>
+          </TableHeader>
+
+          {/* Table Body */}
+          <TableBody className="divide-y divide-gray-100 dark:divide-white/[0.05]">
+            {data.map((order, index) => (
+              <TableRow key={index}>
+                <TableCell className="px-5 py-3 text-gray-500 dark:text-gray-400">
+                  {order.items.map((item, index) => (
+                    <div key={item._id}>
+                      <p className="">
+                        <span>{index + 1}. </span> {item.name} × {item.quantity}
+                      </p>
+                    </div>
+                  ))}
+                </TableCell>
+                <TableCell className="px-5 py-3 text-gray-500 dark:text-gray-400">
+                  <p>
+                    {order.address.firstName + " " + order.address.lastName}
+                  </p>
+                  <p className="text-xs text-gray-500">
+                    {order.address.street}, {order.address.city},{" "}
+                    {order.address.state}, {order.address.country},{" "}
+                    {order.address.zipcode}
+                  </p>
+                  <p className="text-xs">{order.address.phone}</p>
+                </TableCell>
+                <TableCell className="px-5 py-3 text-gray-500 dark:text-gray-400">
+                  ${order.amount}
+                </TableCell>
+                <TableCell className="px-5 py-3 text-gray-500 dark:text-gray-400">
+                  {order.payment ? "Paid" : "Pending"}
+                </TableCell>
+                <TableCell className="px-5 py-3 text-gray-500 dark:text-gray-400">
+                  {order.paymentMethod}
+                </TableCell>
+                <TableCell className="px-5 py-3 text-gray-500 dark:text-gray-400">
+                  {new Date(order.date).toLocaleDateString("en-GB")}
+                </TableCell>
+                <TableCell className="px-5 py-3 text-gray-500 dark:text-gray-400">
+                  <select
+                    onChange={(e) => (e, order._id)}
+                    value={order.status}
+                    className="bg-white border border-gray-300 px-2 py-1 text-xs rounded"
+                  >
+                    <option value="Order Placed">Order Placed</option>
+                    <option value="Packing">Packing</option>
+                    <option value="Shipped">Shipped</option>
+                    <option value="Out for delivery">Out for delivery</option>
+                    <option value="Delivered">Delivered</option>
+                  </select>{" "}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default OrderTable;
--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { backendUrl, currency } from "../../App";
-import { useEffect } from "react";
 import { toast } from "react-hot-toast";
-import { assets } from "../../assets/assets";
 import { useAuth } from "../../context/AuthContext";
 import PageMeta from "../../components/common/PageMeta";
 import ComponentCard from "../../components/common/ComponentCard";
 import PageBreadcrumb from "../../components/common/PageBreadcrumb";
 import Loader from "../../components/common/Loader";
+import OrderTable from "../../components/tables/OrderTable";
 
 const Order = () => {
   const { token } = useAuth();
@@ -16,9 +15,7 @@ const Order = () => {
   const [loading, setLoading] = useState(true);
 
   const fetchAllOrders = async () => {
-    if (!token) {
-      return;
-    }
+    if (!token) return;
     try {
       const response = await axios.post(
         backendUrl + "/api/order/list",
@@ -64,90 +61,12 @@ const Order = () => {
 
   return (
     <>
-      <PageMeta
-        title="React.js Basic Tables Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js Basic Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
-      />
+      <PageMeta title="All Orders" description="Manage customer orders" />
       <PageBreadcrumb pageTitle="All Orders" />
-      <div className="space-y-6">
-        <ComponentCard title={"All Orders"}>
-          <div>
-            <div>
-              {orders.map((order, index) => (
-                <div
-                  className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700 rounded-2xl"
-                  key={index}
-                >
-                  <img className="w-12" src={assets.parcel_icon} alt="" />
-                  <div>
-                    <div>
-                      {order.items.map((item, index) => {
-                        if (index === order.items.length - 1) {
-                          return (
-                            <p className="py-0.5" key={index}>
-                              {" "}
-                              {item.name} X {item.quantity}{" "}
-                              <span> {item.size} </span>
-                            </p>
-                          );
-                        } else {
-                          return (
-                            <p className="py-0.5" key={index}>
-                              {" "}
-                              {item.name} X {item.quantity}{" "}
-                              <span> {item.size} </span> ,
-                            </p>
-                          );
-                        }
-                      })}
-                    </div>
 
-                    <p className="mt-3 mb-2 font-medium">
-                      {order.address.firstName + " " + order.address.lastName}
-                    </p>
-                    <div>
-                      <p>{order.address.street + ", "}</p>
-                      <p>
-                        {order.address.city +
-                          ", " +
-                          order.address.state +
-                          ", " +
-                          order.address.country +
-                          ", " +
-                          order.address.zipcode}
-                      </p>
-                    </div>
-                    <p>{order.address.phone}</p>
-                  </div>
-                  <div>
-                    <p className="text-sm sm:text-[15px]">
-                      Items: {order.items.length}
-                    </p>
-                    <p className="mt-3">Method: {order.paymentMethod}</p>
-                    <p>Payment: {order.payment ? "Done" : "Pending"}</p>
-                    <p>Date: {new Date(order.date).toLocaleDateString()}</p>
-                  </div>
-                  <p className="text-sm sm:text-[15px]">
-                    {currency}
-                    {order.amount}
-                  </p>
-                  <select
-                    onChange={(event) => statusHandler(event, order._id)}
-                    value={order.status}
-                    className="p-2 font-semibold"
-                  >
-                    <option value="Order Placed">Order Placed</option>
-                    <option value="Packing">Packing</option>
-                    <option value="Shipped">Shipped</option>
-                    <option value="Out for delivery">Out for delivery</option>
-                    <option value="Delivered">Delivered</option>
-                  </select>
-                </div>
-              ))}
-            </div>
-          </div>
-        </ComponentCard>
-      </div>
+      <ComponentCard title="All Orders">
+        <OrderTable data={orders}/>
+      </ComponentCard>
     </>
   );
 };
